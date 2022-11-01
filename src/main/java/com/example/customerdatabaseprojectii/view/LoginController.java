84be7e0e09fa771ac40386e1d5d9f1c539f73802
jared <jaredschwery1@gmail.com>
2022-11-01 18:25:14 -0500

    commit2

--- src/main/java/com/example/customerdatabaseprojectii/view/LoginController.java
+++ src/main/java/com/example/customerdatabaseprojectii/view/LoginController.java
@@ -19,11 +19,6 @@ public class LoginController {
     @FXML
     TextField passwordTextEntry;
 
-
-
-
-
-
     public void ButtonClick(ActionEvent event) {
         String username = usernameTextEntry.getText();
         String password = passwordTextEntry.getText();
@@ -36,11 +31,8 @@ public class LoginController {
                 Main.changeScene("src/main/java/com/example/customerdatabaseprojectii/view/AccessGranted.fxml",
                         Main.getMainStage(), 450, 385,"Welcome + ");//get user name from database
             }
-
         }catch (Exception e){
             e.printStackTrace();
         }
-
     }
-
 }
